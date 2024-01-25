420ec69770e98056e11a9c045b86a8ed4ef80d70
xtramer123 <jveliz2205XT@gmail.com>
2024-01-24 19:02:21 -0500

    pyth

---
+++ PROYECTOS/e1.py
@@ -0,0 +1,9 @@
+print("Ingresa tu nombre: ")
+nombre = input()
+print("Ingresa tu edad: ")
+edad = int(input())
+#comentando
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
